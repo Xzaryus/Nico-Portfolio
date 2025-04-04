1fec7864cd4fc270db8c304e88086fab2c0ff149
Xzaryus <nicocodes@hotmail.com>
2025-04-04 14:12:20 +0100

    Card edit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,7 +3,7 @@
 function Card(){
     return(
         <div className="hidden card" id="about">
-            <img src="/Nico-Portfolio/AI-pic.svg" alt="Profile picture" />
+            <img src="./Nico-Portfolio/AI-pic.svg" alt="Profile picture" />
             <h2 className="cardName">Nico</h2>
             <p className="cardText">Aspiring web developer based in the UK. I am passionate about JavaScript, HTML, CSS, and SQL. I love building interactive web solutions and constantly learning new technologies to grow as a developer. </p>
         </div>
