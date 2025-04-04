f0bdb6caedfd3ab0d63f572f9299f7c750c6fcc9
Xzaryus <nicocodes@hotmail.com>
2025-04-04 16:55:14 +0100

    Fix Calc spelling error

--- src/Components/New-projects.jsx
+++ src/Components/New-projects.jsx
@@ -4,7 +4,7 @@ function NewProjects() {
     return (
         <>
             <MyProjects title='Rock-Paper-Scissors game' image="/Nico-Portfolio/RPS.png" alt='Rock-Paper-Scissors game' description='I created this Rock paper Scissors game using HTML, CSS, and JavaScript. It is a fun and interactive way to play against a computer.' />
-            <MyProjects title='Tip calculatior' image="/Nico-Portfolio/Tip-calc.png" alt='Tip calculator' description='I created this tip calculator using HTML, CSS, and JavaScript. It is a simple and easy to use tool for calculating tips.'/>
+            <MyProjects title='Tip calculator' image="/Nico-Portfolio/Tip-calc.png" alt='Tip calculator' description='I created this tip calculator using HTML, CSS, and JavaScript. It is a simple and easy to use tool for calculating tips.'/>
         </>
     )
 }
