fa89377380abc7b6d367f788e1b36ab29e2bac61
Xzaryus <nicocodes@hotmail.com>
2025-04-04 13:18:29 +0100

    Fixing images

--- src/Components/Burger.jsx
+++ src/Components/Burger.jsx
@@ -27,7 +27,7 @@ function Burger() {
     
     return (
         <>
-        <img id="burger-icon" src="src/assets/Burger.svg" alt="Menu" onClick={toggleMenu}/>
+        <img id="burger-icon" src="../assets/Burger.svg" alt="Menu" onClick={toggleMenu}/>
         <nav id="burger-menu" className={menuOpen ? "open" : ""}>
             <ul id="burger-menu-list">
             <li><a href="#top">Home</a></li>
--- src/Components/Stack.jsx
+++ src/Components/Stack.jsx
@@ -3,12 +3,12 @@ function Stack(){
 
     return(
         <ul className="hidden" id="stack">
-            <li><img className="langs" id="pic-1" src="src/assets/html-1.svg" alt="HTML"/></li>
-            <li><img className="langs" id="pic-2" src="src/assets/CSS.svg" alt="CSS"/></li>
-            <li><img className="langs" id="pic-3" src="src/assets/js-logo.svg" alt="JS"/></li>
-            <li><img className="langs" id="pic-4" src="src/assets/logo-mysql.svg" alt="SQL"/></li>
-            <li><img className="langs" id="pic-5" src="src/assets/react.svg" alt="React"/></li>
-            <li><img className="langs" id="pic-6" src="src/assets/python-logo.svg" alt="Python"/></li>         </ul>
+            <li><img className="langs" id="pic-1" src="../assets/html-1.svg" alt="HTML"/></li>
+            <li><img className="langs" id="pic-2" src="../assets/CSS.svg" alt="CSS"/></li>
+            <li><img className="langs" id="pic-3" src="../assets/js-logo.svg" alt="JS"/></li>
+            <li><img className="langs" id="pic-4" src="../assets/logo-mysql.svg" alt="SQL"/></li>
+            <li><img className="langs" id="pic-5" src="../assets/react.svg" alt="React"/></li>
+            <li><img className="langs" id="pic-6" src="../assets/python-logo.svg" alt="Python"/></li>         </ul>
     )
 }
 
