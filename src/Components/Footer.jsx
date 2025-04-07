0dc6e26927c045a858d69bea934dbb01c473cdc7
Xzaryus <nicocodes@hotmail.com>
2025-04-07 13:34:22 +0100

    Added project links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@
 function Footer() {
     return (
         <footer>
-            <p>&copy; {new Date().getFullYear() } My Website. All rights reserved.</p>
+            <p>&copy; {new Date().getFullYear() } Nico. All rights reserved.</p>
             <br></br>
             <p>Inspirational quotes provided by <a href="https://zenquotes.io/" target="_blank">ZenQuotes API</a>
             </p>
--- src/Components/MyProjects.jsx
+++ src/Components/MyProjects.jsx
@@ -4,6 +4,7 @@ function MyProjects(props) {
             <h2>{props.title}</h2>
             <img src={props.image} alt={props.alt} />
             <p>{props.description}</p>
+            <a id="project-link" href={props.link} target="_blank"><button id="project-button">Visit Here</button></a>
         </div>
     )
 }
